02a37562ea18378c7ff55dc0e8e3ab01ca2241f5
Aditya3403 <adityabansal3403@gmail.com>
2025-07-01 12:49:57 +0530

    final hosting issues

--- app/page.tsx
+++ app/page.tsx
@@ -155,6 +155,7 @@ export default function CanvasDesigner() {
     try {
       new URL(url);
     } catch (err) {
+      console.log(err)
       alert('Please enter a valid URL');
       return;
     }
